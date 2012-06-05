454afec62aee1308d88424a36dcff61e38a9c7df
Taeki Ten of {[.NAiLArA:T3K`]} <sourcecode@nailara.network>
2012-06-05 22:25:01 +0200

    importing prototype sourcecode

---
+++ modules/net.cmd
@@ -0,0 +1,30 @@
+# >:]
+
+# name       = net.cmd
+
+
+my ( $filehandle, $command, $handler, $param_hash ) = 
+  (
+      ${$_[0]}{'handle'},
+      ${$_[0]}{'command'},
+      ${$_[0]}{'answer_handler'},
+      ${$_[0]}{'handler_params'}
+  );
+
+if ( not defined ${$_[0]}{'answer_handler'} ) {
+    &{ $code{'base.log'} } ( 0, "net.cmd: no answer-handler specified");
+}
+
+if( not defined ${$_[0]}{'target_cid'})
+{
+    ${$_[0]}{'target_cid'} = &{ $code{'base.gen_id'} }
+    ( \$data{'handle'}{$filehandle}{'cmd'}, 23000 ); # -> config
+}
+
+$data{'handle'}{${$_[0]}{'handle'}}{'cmd'}{${$_[0]}{'target_cid'}}{'handler'} = ${$_[0]}{'answer_handler'};
+$data{'handle'}{${$_[0]}{'handle'}}{'cmd'}{${$_[0]}{'target_cid'}}{'params'} = ${$_[0]}{'handler_params'};
+
+&{ $code{'net.out'} }
+  ( ${$_[0]}{'handle'}, '(' . ${$_[0]}{'target_cid'} . ')' . ${$_[0]}{'command'} . "\n" );
+
+# return $command_id;
