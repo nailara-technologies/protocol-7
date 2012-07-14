2c9c9c6973c6d4421ed6481a3ce250682e2589b8
Taeki Ten of {[.NAiLArA:T3K`]} <sourcecode@nailara.network>
2012-07-14 23:23:16 +0200

    comitted ./bin/cpan_install.debian.sh script

---
+++ bin/cpan_install.debian.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# installs cpanm and some basic (packaged) cpan modules
+apt-get install cpanminus libevent-perl libdigest-sha-perl\
+  libio-all-perl libcrypt-cbc-perl libcrypt-openssl-random-perl\
+  libcrypt-openssl-rsa-perl
+
+# installs Crypt::Twofish2 (for which no package exists)
+cpanm install Crypt::Twofish2
