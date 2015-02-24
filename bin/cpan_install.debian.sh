4f3f30a8a0370e4025340d1f037240f1378e1d39
Taeki Ten of {[.NAiLArA:T3K`]} <taeki.ten@protocol-7.network>
2015-02-24 13:38:31 +0100

    addedd Text::Lorem to dependency list..

--- bin/cpan_install.debian.sh
+++ bin/cpan_install.debian.sh
@@ -4,7 +4,7 @@
 apt-get install cpanminus libevent-perl libdigest-sha-perl\
   libio-all-perl libcrypt-cbc-perl libcrypt-openssl-random-perl\
   libcrypt-openssl-rsa-perl libproc-processtable-perl perl-tk\
-  libsdl-perl
+  libsdl-perl libtext-lorem-perl
 
 # installs Crypt::Twofish2 (for which no package exists)
 cpanm install Crypt::Twofish2 X11::Tops
