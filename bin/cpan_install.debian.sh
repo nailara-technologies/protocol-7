bf1ea582c2ed179dfdacee086f0805c880d12964
Taeki Ten of {[.NAiLArA:T3K`]} <taeki.ten@protocol-7.network>
2015-02-24 11:06:09 +0100

    updated dependencies in installer scripts..

--- bin/cpan_install.debian.sh
+++ bin/cpan_install.debian.sh
@@ -3,7 +3,8 @@
 # installs cpanm and some basic (packaged) cpan modules
 apt-get install cpanminus libevent-perl libdigest-sha-perl\
   libio-all-perl libcrypt-cbc-perl libcrypt-openssl-random-perl\
-  libcrypt-openssl-rsa-perl
+  libcrypt-openssl-rsa-perl libproc-processtable-perl perl-tk\
+  libsdl-perl
 
 # installs Crypt::Twofish2 (for which no package exists)
-cpanm install Crypt::Twofish2
+cpanm install Crypt::Twofish2 X11::Tops
