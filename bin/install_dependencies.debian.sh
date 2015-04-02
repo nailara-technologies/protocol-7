5fd45a8253e023b50f7b3a20e3d4167649fe0a62
Taeki Ten of {[.NAiLArA:T3K`]} <taeki.ten@protocol-7.network>
2015-04-02 16:22:20 +0200

    mpv player agent (part 1)

--- bin/install_dependencies.debian.sh
+++ bin/install_dependencies.debian.sh
@@ -2,4 +2,4 @@
 
 apt-get -y install notify-osd x11-utils x11-xserver-utils unclutter\
                 fonts-droid compton openbox hsetroot xserver-xorg\
-                xserver-xephyr xvfb
+                xserver-xephyr xvfb mpv
