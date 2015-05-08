80e6cca297eeba6c05f78bc306ad2bcfd537641a
Taeki Ten of {[.NAiLArA:T3K`]} <taeki.ten@protocol-7.network>
2015-05-08 11:59:56 +0200

    committed weather agent (part 1)

--- bin/cpan_install.debian.sh
+++ bin/cpan_install.debian.sh
@@ -10,10 +10,10 @@ apt-get install cpanminus libevent-perl libdigest-sha-perl\
   libterm-readline-perl-perl libx11-protocol-perl\
   libx11-protocol-other-perl libfreezethaw-perl\
   libwebkitgtk-dev libjson-perl libpoppler-glib-dev \
-  libgraphics-magick-perl libimage-exif-perl
+  libgraphics-magick-perl libimage-exif-perl libfreezethaw-perl
   #libtext-unidecode-perl
   #libconvert-uu-perl
 
 # installs the rest from cpan (note: Gtk2::WebKit needs running X-server)
 cpanm Crypt::Twofish2 X11::Tops Gtk2::WebKit XML::RSS::TimingBot\
-      XML::RSS::Parser File::MimeInfo::Magic Poppler
+      XML::RSS::Parser File::MimeInfo::Magic Poppler Astro::MoonPhase
