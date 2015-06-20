8bcea39d5a2ad5f07c122711563b3400526274c4
Taeki Ten of {[.NAiLArA:T3K`]} <taeki.ten@protocol-7.network>
2015-06-21 01:42:33 +0200

    imported config.json module (part 1)

--- bin/cpan_install.debian.sh
+++ bin/cpan_install.debian.sh
@@ -7,9 +7,9 @@ apt-get install cpanminus libevent-perl libdigest-sha-perl\
   libsdl-perl libtext-lorem-perl libevent-perl\
   libcrypt-openssl-random-perl libproc-processtable-perl\
   libglib-perl libgtk2-perl libterm-readpassword-perl\
-  libterm-readline-perl-perl libx11-protocol-perl\
-  libx11-protocol-other-perl libfreezethaw-perl\
-  libwebkitgtk-dev libjson-perl libpoppler-glib-dev \
+  libterm-readline-perl-perl libx11-protocol-perl libhash-flatten-perl\
+  libx11-protocol-other-perl libfreezethaw-perl libclone-perl\
+  libhash-merge-simple-perl libwebkitgtk-dev libjson-perl libpoppler-glib-dev \
   libgraphics-magick-perl libimage-exif-perl libfreezethaw-perl
   #libtext-unidecode-perl
   #libconvert-uu-perl
