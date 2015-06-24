adacf303181a4ac2f39aadc87b312221a1073f18
Taeki Ten of {[.NAiLArA:T3K`]} <taeki.ten@protocol-7.network>
2015-06-24 10:10:59 +0200

    filesystem (mount) agent [simple version]

--- bin/cpan_install.debian.sh
+++ bin/cpan_install.debian.sh
@@ -9,7 +9,7 @@ apt-get install cpanminus libevent-perl libdigest-sha-perl\
   libglib-perl libgtk2-perl libterm-readpassword-perl\
   libterm-readline-perl-perl libx11-protocol-perl libhash-flatten-perl\
   libx11-protocol-other-perl libfreezethaw-perl libclone-perl\
-  libhash-merge-simple-perl libwebkitgtk-dev libjson-perl libpoppler-glib-dev \
+  libhash-merge-simple-perl libwebkitgtk-dev libjson-perl libpoppler-glib-dev\
   libgraphics-magick-perl libimage-exif-perl libfreezethaw-perl
   #libtext-unidecode-perl
   #libconvert-uu-perl
