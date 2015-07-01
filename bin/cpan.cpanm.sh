1141befdea484cc7e9f2643bb18fd631c735d4af
Taeki Ten of {[.NAiLArA:T3K`]} <taeki.ten@protocol-7.network>
2015-07-01 08:10:07 +0200

    fixes / comments in ./bin/cpan.cpanm.sh ...

--- bin/cpan.cpanm.sh
+++ bin/cpan.cpanm.sh
@@ -9,11 +9,16 @@ cpanm Event Clone Hash::Flatten Hash::Merge::Simple \
       Crypt::CBC \
       JSON JSON::PP \
       Net::SSH2 \
-      LWP::UserAgent File::MimeInfo::Magic \
+      LWP::UserAgent LWP::Protocol::https File::MimeInfo::Magic \
       X11::Protocol X11::Protocol::WM X11::Tops \
-      Graphics::Magick FreezeThaw \
+      FreezeThaw \
       XML::RSS::TimingBot XML::RSS::Parser \
       SDL \
       Crypt::Twofish2 \
       HTTP::Request \
       Gtk2::WebKit
+
+# note: XML::SAX failed with cpanm ... installation with 'cpan' itself worked
+
+# Graphics::Magick : http://sourceforge.net/projects/graphicsmagick/files/graphicsmagick/
+#                    ..or check your distribution packages!
