1c9de1e6c6bccdc101270265f2d36cdb23b10364
Taeki Ten of {[.NAiLArA:T3K`]} <taeki.ten@protocol-7.network>
2015-07-03 08:18:52 +0200

    added './bin/install_minimal_dependencies.debian.sh' script

---
+++ bin/install_minimal_dependencies.debian.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+NAILARA_ROOT=/usr/local/nailara
+
+useradd -r nailara
+ln -v -s $NAILARA_ROOT/bin/nailara /usr/bin/nailara
+ln -v -s $NAILARA_ROOT/bin/nshell /usr/bin/nshell
+
+cp -v $NAILARA_ROOT/lib/systemd/system/nailara-root.service /lib/systemd/system/
+
+# dependencies for 'root','core','config' agents + nshell
+
+apt-get -y install libevent-perl libcrypt-openssl-random-perl \
+  libproc-processtable-perl libterm-readpassword-perl \
+  libterm-readline-perl-perl libdigest-sha-perl \
+  libhash-flatten-perl libhash-merge-simple-perl libjson-pp-perl
