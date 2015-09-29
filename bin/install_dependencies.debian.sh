5c0433facfff9852a97e574f72daf35ed3ba6102
{[.NAiLArA:T3k`]} <src@dev.nailara.net>
2015-09-29 18:37:09 +0200

    implemented image viewer agent using 'impressive' python script

--- bin/install_dependencies.debian.sh
+++ bin/install_dependencies.debian.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 apt-get -y install notify-osd x11-utils x11-xserver-utils unclutter\
-                fonts-droid compton openbox hsetroot xserver-xorg\
-                xserver-xephyr xvfb mpv ffmpeg expect mediainfo
+            fonts-droid compton openbox hsetroot xserver-xorg\
+            xserver-xephyr xvfb mpv ffmpeg expect mediainfo\
+            python-imaging python-pygame mupdf-tools pdftk # <- impressive agent
