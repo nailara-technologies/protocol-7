b013ee72dbb1c9cc4f4d9cd73e7c04e7eaf3e049
Taeki Ten of {[.NAiLArA:T3K`]} <taeki.ten@protocol-7.network>
2015-10-05 14:02:32 +0200

    added 'info' + 'warn' command to notify agent

--- bin/install_dependencies.debian.sh
+++ bin/install_dependencies.debian.sh
@@ -3,4 +3,5 @@
 apt-get -y install notify-osd x11-utils x11-xserver-utils unclutter\
             fonts-droid compton openbox hsetroot xserver-xorg\
             xserver-xephyr xvfb mpv ffmpeg expect mediainfo\
-            python-imaging python-pygame mupdf-tools pdftk # <- impressive agent
+            python-imaging python-pygame mupdf-tools pdftk
+            oxygen-icon-theme dbus-x11 libnotify-bin notify-osd
