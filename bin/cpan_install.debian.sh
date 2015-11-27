c38df2a4e4d505d41584b979aac1b0fab59f9647
Taeki Ten of {[.NAiLArA:T3K`]} <sourcecode@nailara.network>
2015-11-27 22:40:08 +0100

    added http proxy support to browser agent

--- bin/cpan_install.debian.sh
+++ bin/cpan_install.debian.sh
@@ -21,4 +21,4 @@ apt-get install cpanminus libevent-perl libdigest-sha-perl\
 # installs the rest from cpan (note: Gtk3::WebKit needs X-server or Xvfb)
 cpanm Crypt::Twofish2 X11::Tops X11::Protocol::WM Gtk3::WebKit \
       XML::RSS::TimingBot XML::RSS::Parser File::MimeInfo::Magic \
-      Mediainfo Poppler
+      HTTP::Soup Mediainfo Poppler
