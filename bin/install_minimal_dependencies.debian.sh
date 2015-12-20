aee60c9fb1ae50fe8726142b898a408021913652
Taeki Ten of {[.NAiLArA:T3K`]} <taeki.ten@protocol-7.network>
2015-12-20 23:14:00 +0100

    replaced Crypt::OpenSSL::Random module with Crypt::PRNG::Fortuna

--- bin/install_minimal_dependencies.debian.sh
+++ bin/install_minimal_dependencies.debian.sh
@@ -10,7 +10,6 @@ cp -v $NAILARA_ROOT/lib/systemd/system/nailara-root.service /lib/systemd/system/
 
 # dependencies for 'root','core','config' agents + nshell
 
-apt-get -y install libevent-perl libcrypt-openssl-random-perl \
-  libproc-processtable-perl libterm-readpassword-perl \
-  libterm-readline-perl-perl libdigest-sha-perl libclone-perl \
-  libhash-flatten-perl libhash-merge-simple-perl libjson-xs-perl
+apt-get -y install libevent-perl libproc-processtable-perl \
+  libterm-readpassword-perl libterm-readline-perl-perl libdigest-sha-perl \
+  libclone-perl libhash-flatten-perl libhash-merge-simple-perl libjson-xs-perl
