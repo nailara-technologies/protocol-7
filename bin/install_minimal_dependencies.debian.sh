0ef4d7074972a2b967be0bd23ef392b033f9851b
{[.NAiLArA:T3k`]} <src@dev.nailara.net>
2015-12-21 00:53:22 +0100

    added Crypt::PRNG::Fortuna to 'install_minimal_dependencies.debian.sh'

--- bin/install_minimal_dependencies.debian.sh
+++ bin/install_minimal_dependencies.debian.sh
@@ -10,6 +10,8 @@ cp -v $NAILARA_ROOT/lib/systemd/system/nailara-root.service /lib/systemd/system/
 
 # dependencies for 'root','core','config' agents + nshell
 
-apt-get -y install libevent-perl libproc-processtable-perl \
+apt-get -y install cpanminus libevent-perl libproc-processtable-perl \
   libterm-readpassword-perl libterm-readline-perl-perl libdigest-sha-perl \
   libclone-perl libhash-flatten-perl libhash-merge-simple-perl libjson-xs-perl
+
+cpanm Crypt::PRNG::Fortuna
