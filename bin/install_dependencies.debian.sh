56c6ce93e31bfcda3e2bd2f2f9b03fd17953d851
Taeki Ten of {[.NAiLArA:T3K`]} <sourcecode@nailara.network>
2016-07-08 22:27:09 +0200

    added Date::Parse to cpan module dependencies (required by 'events' agent)

--- bin/install_dependencies.debian.sh
+++ bin/install_dependencies.debian.sh
@@ -3,8 +3,8 @@
 apt-get -y install notify-osd x11-utils x11-xserver-utils unclutter\
             fonts-droid compton openbox hsetroot xserver-xorg\
             xserver-xephyr xvfb mpv ffmpeg expect mediainfo\
-            python-imaging python-pygame mupdf-tools pdftk \
-            gnome-colors-common oxygen-icon-theme \
-            dbus-x11 libnotify-bin notify-osd intel-gpu-tools \
-            liblwpx-paranoidagent-perl libnet-arp-perl \
+            python-imaging python-pygame mupdf-tools pdftk\
+            gnome-colors-common oxygen-icon-theme\
+            dbus-x11 libnotify-bin notify-osd intel-gpu-tools\
+            liblwpx-paranoidagent-perl libnet-arp-perl libtimedate-perl\
             libimlib2-dev libfile-mimeinfo-perl liblinux-inotify2-perl
