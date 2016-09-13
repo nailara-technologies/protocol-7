08c08a0dba1163f12844c594ac874b507c492881
Taeki Ten of {[.NAiLArA:T3K`]} <taeki.ten@protocol-7.network>
2016-09-13 16:24:20 +0200

    removed apt-get upgrade option that doesn't work on debian jessie

--- bin/debian_dist_upgrade.sh
+++ bin/debian_dist_upgrade.sh
@@ -13,10 +13,11 @@ dpkg --configure -a ; apt-get -fy install # [automatic recovery, if required]
 
 apt-get update && \
 apt-get -fuy \
-	--no-allow-insecure-repositories \
 	-o Dpkg::Options::="--force-confnew" \
 	-o Dpkg::Options::="--force-confdef" \
 	-o Dpkg::Options::="--force-overwrite" dist-upgrade && \
 	apt-get clean && \
 	apt-get -y --purge autoremove && \
 	echo "\n:\n: done.\n:\n"
+
+# --no-allow-insecure-repositories \ # <- meh // debian jessie --no-likes-it ;)
