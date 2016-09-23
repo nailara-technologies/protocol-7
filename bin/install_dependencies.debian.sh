709db22156e541020298c77b1e08aad408c1edf4
Taeki Ten of {[.NAiLArA:T3K`]} <taeki.ten@protocol-7.network>
2016-09-24 00:34:52 +0200

    updating dependency install scripts

--- bin/install_dependencies.debian.sh
+++ bin/install_dependencies.debian.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 apt-get -y install notify-osd x11-utils x11-xserver-utils unclutter\
-            fonts-droid compton openbox hsetroot xserver-xorg\
+            compton openbox hsetroot xserver-xorg\
             xserver-xephyr xvfb mpv ffmpeg expect mediainfo\
             python-imaging python-pygame mupdf-tools pdftk\
             gnome-colors-common oxygen-icon-theme\
