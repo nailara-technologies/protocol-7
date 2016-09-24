d75a4f62a625c1ffb0502b70849776b9875b5317
{[.NAiLArA:T3k`]} <src@dev.nailara.net>
2016-09-24 23:44:09 +0200

    made intel-gpu-tools package "optional" in 'install_dependencies.debian.sh'

--- bin/install_dependencies.debian.sh
+++ bin/install_dependencies.debian.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 apt-get -y install notify-osd x11-utils x11-xserver-utils unclutter\
-            compton openbox hsetroot xserver-xorg\
-            xserver-xephyr xvfb mpv ffmpeg expect mediainfo\
+            compton openbox hsetroot xserver-xorg xserver-xephyr xvfb\
+            mpv ffmpeg expect mediainfo dbus-x11 libnotify-bin notify-osd\
             python-imaging python-pygame mupdf-tools pdftk\
             gnome-colors-common oxygen-icon-theme\
-            dbus-x11 libnotify-bin notify-osd intel-gpu-tools\
             liblwpx-paranoidagent-perl libnet-libdnet-perl libtimedate-perl\
             libimlib2-dev libfile-mimeinfo-perl liblinux-inotify2-perl \
             libxml-rsslite-perl
+
+# optional [i.e. not on raspbian]
+apt-get -y install intel-gpu-tools 2>/dev/null
