8046b592ebdc2eb727e0a510ed7951ba1f69dc4a
Taeki Ten of {[.NAiLArA:T3K`]} <sourcecode@nailara.network>
2016-09-24 23:23:46 +0200

    apt-get action now configurable in 'debian_dist_upgrade.sh'

--- bin/debian_dist_upgrade.sh
+++ bin/debian_dist_upgrade.sh
@@ -1,4 +1,8 @@
-#!/bin/sh
+#!/bin/bash
+
+source /etc/os-release
+
+export ACTION=dist-upgrade # upgrade | dist-upgrade
 
 export PATH=/usr/sbin:/usr/bin:/sbin:/bin
 
@@ -6,7 +10,7 @@ export DEBIAN_FRONTEND=noninteractive
 export APT_LISTCHANGES_FRONTEND=none
 export PAGER=/bin/true
 
-echo "\n:\n: starting dist-upgrade ...\n:\n"
+echo -e "\n:\n: starting $ID $ACTION ...\n:\n"
 
 
 dpkg --configure -a ; apt-get -fy install # [automatic recovery, if required]
@@ -15,9 +19,7 @@ apt-get update && \
 apt-get -fuy \
 	-o Dpkg::Options::="--force-confnew" \
 	-o Dpkg::Options::="--force-confdef" \
-	-o Dpkg::Options::="--force-overwrite" dist-upgrade && \
+	-o Dpkg::Options::="--force-overwrite" $ACTION && \
 	apt-get clean && \
 	apt-get -y --purge autoremove && \
-	echo "\n:\n: done.\n:\n"
-
-# --no-allow-insecure-repositories \ # <- meh // debian jessie --no-likes-it ;)
+	echo -e "\n:\n: done.\n:\n"
