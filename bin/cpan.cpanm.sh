22cf6334c491b64ef2414ef5df56e86c100b0993
{[.NAiLArA:T3k`]} <src@dev.nailara.net>
2016-10-03 04:02:55 +0200

    added File::Slurper dependency [not used yet]

--- bin/cpan.cpanm.sh
+++ bin/cpan.cpanm.sh
@@ -4,7 +4,7 @@ export PERL5LIB=. # <- for HTTP::Soup
 
 xvfb-run cpanm Event Clone Hash::Flatten Hash::Merge::Simple \
       Term::ReadPassword Term::ReadKey Term::ReadLine::Perl \
-      POSIX::1003 Proc::ProcessTable Date::Parse \
+      POSIX::1003 Proc::ProcessTable Date::Parse File::Slurper \
       LWPx::ParanoidAgent \
       Sys::Filesystem \
       Crypt::CBC CryptX \
