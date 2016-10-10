cfc8a1853f6ff76109d0916069c1aa206fb3267b
Taeki Ten of {[.NAiLArA:T3K`]} <sourcecode@nailara.network>
2016-10-10 17:01:34 +0200

    implementing "discover" agent [PART 1]

--- bin/install_dependencies.debian.sh
+++ bin/install_dependencies.debian.sh
@@ -4,7 +4,7 @@ apt-get -y install notify-osd x11-utils x11-xserver-utils unclutter\
             compton openbox hsetroot xserver-xorg xserver-xephyr xvfb\
             mpv ffmpeg expect mediainfo dbus-x11 libnotify-bin notify-osd\
             python-imaging python-pygame mupdf-tools pdftk\
-            gnome-colors-common oxygen-icon-theme\
+            libio-socket-multicast-perl gnome-colors-common oxygen-icon-theme\
             liblwpx-paranoidagent-perl libnet-libdnet-perl libtimedate-perl\
             libimlib2-dev libfile-mimeinfo-perl liblinux-inotify2-perl \
             libxml-rsslite-perl libtest-needsdisplay-perl libfile-slurper-perl
