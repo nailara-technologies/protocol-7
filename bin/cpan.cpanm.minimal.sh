cfb6daa76d49e1f68de67e0816f3824c43b14baa
Taeki Ten of {[.NAiLArA:T3K`]} <taeki.ten@protocol-7.network>
2016-10-14 00:16:12 +0200

    added 'events'-agent dependencies to cpan.cpanm.minimal.sh

--- bin/cpan.cpanm.minimal.sh
+++ bin/cpan.cpanm.minimal.sh
@@ -3,4 +3,5 @@
 cpanm Event POSIX::1003 IO::Handle::Record JSON::XS\
         Crypt::Ed25519 Crypt::Curve25519 Digest::BLAKE2\
         Clone Hash::Flatten Hash::Merge::Simple CryptX\
-        Proc::ProcessTable Term::ReadKey Term::ReadPassword Term::ReadLine::Perl
+        Date::Parse Proc::ProcessTable\
+        Term::ReadKey Term::ReadPassword Term::ReadLine::Perl
