621dfaa58966d95a16a0ac066387c79adee99bec
Taeki Ten of {[.NAiLArA:T3K`]} <sourcecode@nailara.network>
2016-10-17 14:47:37 +0200

    added ticker-gtk agent text outline color feature [enabled by default]

--- bin/install_dependencies.debian.sh
+++ bin/install_dependencies.debian.sh
@@ -6,8 +6,9 @@ apt-get -y install notify-osd x11-utils x11-xserver-utils unclutter\
             python-imaging python-pygame mupdf-tools pdftk\
             libio-socket-multicast-perl gnome-colors-common oxygen-icon-theme\
             liblwpx-paranoidagent-perl libnet-libdnet-perl libtimedate-perl\
-            libimlib2-dev libfile-mimeinfo-perl liblinux-inotify2-perl \
-            libxml-rsslite-perl libtest-needsdisplay-perl libfile-slurper-perl
+            libimlib2-dev libfile-mimeinfo-perl liblinux-inotify2-perl\
+            libxml-rsslite-perl libtest-needsdisplay-perl libfile-slurper-perl\
+            libconvert-color-perl
 
 # optional [i.e. not on raspbian]
 apt-get -y install intel-gpu-tools 2>/dev/null
