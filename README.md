d2bd9c2b470ba898553d7d95f12f2d257380a41b
Taeki Ten of {[.NAiLArA:T3K`]} <taeki.ten@protocol-7.network>
2017-11-14 03:09:59 +0100

    added "cpu_load" command to 'system' agent

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 # [ [nailara project](http://www.nailara.net/) ]
 
-### [ source code version : 17.8708-2416.0 ]
+### [ source code version : 17.8739-2417.0 ]
 
 ### ( this is the [ISC license](license)d "master" branch )
 ---
--- bin/install_minimal_dependencies.debian.sh
+++ bin/install_minimal_dependencies.debian.sh
@@ -14,11 +14,12 @@ apt-get -y install cpanminus libevent-perl libproc-processtable-perl \
   libterm-readpassword-perl libterm-readline-perl-perl libdigest-sha-perl \
   libclone-perl libhash-flatten-perl libhash-merge-simple-perl libjson-xs-perl \
   libio-socket-multicast-perl libfile-slurper-perl libtimedate-perl gcc make \
-  libio-all-perl libdigest-crc-perl libclass-accessor-lite-perl libc6-dev
+  libio-all-perl libdigest-crc-perl libclass-accessor-lite-perl libc6-dev \
+  libbsd-resource-perl
 
 cpanm CryptX POSIX::1003 Crypt::Curve25519 Crypt::Ed25519 Digest::Skein \
       https://github.com/gitpan/Module-Build-Pluggable-XSUtil.git Digest::BLAKE2 \
-      File::MimeInfo::Magic
+      File::MimeInfo::Magic Sys::Statistics::Linux::CpuStats
 
 
 # XXX cpan2deb --recursive --build --install-deps --install-build-deps --install
