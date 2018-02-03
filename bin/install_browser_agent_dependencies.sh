a626597be57d40fd8695564a7d58c555812c6ea1
Taeki Ten of {[.NAiLArA:T3K`]} <sourcecode@nailara.network>
2018-02-03 21:22:19 +0100

    removed 'io_all.file' module (and IO::All dependency)

--- bin/install_browser_agent_dependencies.sh
+++ bin/install_browser_agent_dependencies.sh
@@ -8,7 +8,7 @@ apt-get -y install pciutils cpanminus libevent-perl libproc-processtable-perl \
   libterm-readpassword-perl libterm-readline-perl-perl libdigest-sha-perl \
   libclone-perl libhash-flatten-perl libhash-merge-simple-perl libjson-xs-perl \
   libio-socket-multicast-perl libfile-slurper-perl libtimedate-perl gcc make \
-  libio-all-perl libdigest-crc-perl libclass-accessor-lite-perl libc6-dev \
+  libdigest-crc-perl libclass-accessor-lite-perl libc6-dev \
   libgtk3-perl libx11-protocol-perl libx11-protocol-other-perl \
   libx11-keyboard-perl liblinux-inotify2-perl openbox hsetroot xvfb \
   libwebkitgtk-3.0-dev libtest-needsdisplay-perl
