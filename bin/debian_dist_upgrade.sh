d6597df12cc5a8b83246bb57b2d897b5e4c57705
Taeki Ten of {[.NAiLArA:T3K`]} <sourcecode@nailara.network>
2018-02-22 14:14:00 +0100

    more unattended debian upgrade fixes

--- bin/debian_dist_upgrade.sh
+++ bin/debian_dist_upgrade.sh
@@ -17,9 +17,11 @@ echo -e "\n:\n: starting $ID $ACTION ...\n:\n"
 dpkg --force-confold --force-confdef --force-confmiss --force-overwrite \
     --configure -a ; apt-get -fy install # [automatic recovery, if required]
 
-apt -y $ACTION
+apt-get -y $ACTION
 
 pam-auth-update --force
+
+rm -rf /var/cache/apt/mediainfo_tmp*
 
 apt-get update && \
 apt-get -fy \
@@ -29,5 +31,7 @@ apt-get -fy \
 	-o Dpkg::Options::="--force-overwrite" $ACTION && \
 	apt-get clean && apt-get -y --purge autoremove && \
 	echo -e "\n:\n: done.\n:\n"
+
+rm -rf /var/cache/apt/mediainfo_tmp*
 
 # dpkg -l | grep '^rc' | awk '{print $2}' | xargs dpkg --purge 2>/dev/null
