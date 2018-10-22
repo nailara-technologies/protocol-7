627e8b0fc6d76823f9146adb692f94e35b10f6aa
{[.NAiLArA:T3k`]} <src@dev.nailara.net>
2018-10-23 00:41:07 +0200

    updated install_minimal_dependencies.debian.sh (now CryptX is a .deb etc.)

--- bin/install_minimal_dependencies.debian.sh
+++ bin/install_minimal_dependencies.debian.sh
@@ -3,8 +3,8 @@
 NAILARA_ROOT=/usr/local/nailara
 
 useradd -m -r nailara
-ln -v -s $NAILARA_ROOT/bin/nailara /usr/bin/nailara
-ln -v -s $NAILARA_ROOT/bin/nshell /usr/bin/nshell
+ln -v -f -s $NAILARA_ROOT/bin/nailara /usr/bin/nailara
+ln -v -f -s $NAILARA_ROOT/bin/nshell /usr/bin/nshell
 
 cp -v $NAILARA_ROOT/lib/systemd/system/nailara-root.service /lib/systemd/system/
 
@@ -15,11 +15,10 @@ apt-get -y install cpanminus libevent-perl libproc-processtable-perl \
   libclone-perl libhash-flatten-perl libhash-merge-simple-perl libjson-xs-perl \
   libio-socket-multicast-perl libfile-slurper-perl libtimedate-perl gcc make \
   libdigest-crc-perl libclass-accessor-lite-perl libc6-dev \
-  liburi-perl libbsd-resource-perl \
-  libtest-requires-perl libtest-sharedfork-perl libmodule-build-pluggable-perl
+  liburi-perl libbsd-resource-perl libcryptx-perl \
+  libtest-requires-perl libtest-sharedfork-perl libmodule-build-pluggable-perl &&
 
-cpanm CryptX POSIX::1003 Crypt::Curve25519 Crypt::Ed25519 Digest::Skein \
-      https://github.com/gitpan/Module-Build-Pluggable-XSUtil.git Digest::BLAKE2 \
+cpanm POSIX::1003 Crypt::Curve25519 Crypt::Ed25519 Digest::Skein Digest::BLAKE2 \
       File::MimeInfo::Magic Sys::Statistics::Linux::CpuStats URI::QueryParam
 
 
