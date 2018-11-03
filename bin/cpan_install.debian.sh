33644cd3f35037224b7d3a5f0a1b2a6224ad53bc
Taeki Ten of {[.NAiLArA:T3K`]} <sourcecode@nailara.network>
2018-11-03 06:15:38 +0100

    preparing switch from Crypt::CBC to Crypt::Mode::CBC module + updating deps

--- bin/cpan_install.debian.sh
+++ bin/cpan_install.debian.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
 
 # installs cpanm and some basic (packaged) cpan modules
-apt-get install xvfb cpanminus libevent-perl libdigest-sha-perl\
-  libcrypt-cbc-perl libproc-processtable-perl libdigest-crc-perl\
-  libsdl-perl libtext-lorem-perl libevent-perl liblwpx-paranoidagent-perl\
-  libproc-processtable-perl libglib-perl libterm-readpassword-perl\
-  libterm-readline-perl-perl libx11-protocol-perl libhash-flatten-perl\
-  libx11-protocol-other-perl libx11-keyboard-perl\
-  libfreezethaw-perl libclone-perl libtimedate-perl\
-  libhash-merge-simple-perl libwebkitgtk-dev libjson-xs-perl\
-  libpoppler-glib-dev libgraphics-magick-perl libimage-exif-perl\
+apt-get install xvfb cpanminus libevent-perl libdigest-sha-perl \
+  libcryptx-perl libproc-processtable-perl libdigest-crc-perl \
+  libsdl-perl libtext-lorem-perl libevent-perl liblwpx-paranoidagent-perl \
+  libproc-processtable-perl libglib-perl libterm-readpassword-perl \
+  libterm-readline-perl-perl libx11-protocol-perl libhash-flatten-perl \
+  libx11-protocol-other-perl libx11-keyboard-perl libtest-needsdisplay-perl \
+  libfreezethaw-perl libclone-perl libtimedate-perl \
+  libhash-merge-simple-perl libwebkitgtk-dev libjson-xs-perl \
+  libpoppler-glib-dev libgraphics-magick-perl libimage-exif-perl \
   libfile-mimeinfo-perl libnet-ssh2-perl libxml-feed-perl libxml-rsslite-perl \
   libgirepository1.0-dev pkg-config libgtk-3-dev libfile-slurper-perl \
   libglib2.0-dev libglib2.0-0 gir1.2-webkit-3.0 \
   libgtk3-perl libgtk2-perl liblinux-inotify2-perl libnet-libdnet-perl \
   libclass-accessor-lite-perl libio-stringy-perl \
-  libimager-perl libfont-freetype-perl
+  libimager-perl libfont-freetype-perl liblwp-useragent-determined-perl
 
   #libtext-unidecode-perl
   #libconvert-uu-perl
 
 # installs the rest from cpan
-cpanm   CryptX Crypt::Curve25519 Crypt::Ed25519 Digest::Skein \
+cpanm   Crypt::Curve25519 Crypt::Ed25519 Digest::Skein \
         POSIX::1003 XML::RSS::TimingBot File::MimeInfo::Magic \
         Config::Hosts HTTP::Soup Mediainfo Poppler Device::Gembird \
         Imager::Screenshot
