c8ccf1e1a92e197908df22812834c4eda7973bfa
Taeki Ten of {[.NAiLArA:T3K`]} <taeki.ten@protocol-7.network>
2018-11-04 23:24:09 +0100

    updated './bin/install_browser_agent_dependencies.sh' script

--- bin/install_browser_agent_dependencies.sh
+++ bin/install_browser_agent_dependencies.sh
@@ -8,14 +8,16 @@ apt-get -y install pciutils cpanminus libevent-perl libproc-processtable-perl \
   libterm-readpassword-perl libterm-readline-perl-perl libdigest-sha-perl \
   libclone-perl libhash-flatten-perl libhash-merge-simple-perl libjson-xs-perl \
   libio-socket-multicast-perl libfile-slurper-perl libtimedate-perl gcc make \
-  libdigest-crc-perl libclass-accessor-lite-perl libc6-dev \
+  libdigest-crc-perl libclass-accessor-lite-perl libc6-dev libglib-perl \
   libgtk3-perl libx11-protocol-perl libx11-protocol-other-perl \
   libx11-keyboard-perl liblinux-inotify2-perl openbox hsetroot xvfb \
-  libwebkitgtk-3.0-dev libtest-needsdisplay-perl
+  libwebkitgtk-3.0-dev libtest-needsdisplay-perl libcryptx-perl \
+  libextutils-pkgconfig-perl libextutils-depends-perl
 
-cpanm CryptX POSIX::1003 \
-      Crypt::Curve25519 Crypt::Ed25519 \
+cpanm Crypt::Curve25519 Crypt::Ed25519 \
       Digest::Skein Glib::Event
+
+cpanm --force POSIX::1003 # <- temporary! ( localtime test fails.. )
 
 #>-- HTTP::Soup
 export PERL5LIB=.
