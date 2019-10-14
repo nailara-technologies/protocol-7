1f1f49ef86a35fd67a13ff4c2f67ec7c89593f52
Taeki Ten of {[.NAiLArA:T3K`]} <sourcecode@nailara.network>
2019-10-14 13:51:18 +0200

    added missing and new dependencies to './bin/install_dependencies.debian.sh'

--- bin/install_dependencies.debian.sh
+++ bin/install_dependencies.debian.sh
@@ -4,7 +4,7 @@
 #         #####################################
 
 apt-get -y install notify-osd x11-utils x11-xserver-utils unclutter \
-            compton openbox hsetroot xserver-xorg xserver-xephyr xvfb \
+            compton openbox hsetroot xserver-xorg xserver-xephyr xvfb fuse3 \
             mpv ffmpeg melt expect mediainfo dbus-x11 libnotify-bin notify-osd \
             python-pil python-pygame mupdf-tools pdftk libgtk3-perl \
             libio-socket-multicast-perl gnome-colors-common oxygen-icon-theme \
@@ -14,6 +14,8 @@ apt-get -y install notify-osd x11-utils x11-xserver-utils unclutter \
             libconvert-color-perl libio-stringy-perl libdigest-crc-perl \
             libdigest-jhash-perl libfont-freetype-perl \
             libfile-mimeinfo-perl shared-mime-info
+
+cpanm --force XML::RSS::Timing  # <- temporary forced [date_conv test fails] <!>
 
 # optional [i.e. not on raspbian]
 apt-get -y install intel-gpu-tools 2>/dev/null
