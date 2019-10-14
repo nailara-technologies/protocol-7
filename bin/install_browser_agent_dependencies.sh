48e92a0f8e5522866a5ebf0d33d53c11846f5ee7
{[.NAiLArA:T3K`]} <src@dev.nailara.net>
2019-10-14 17:01:15 +0200

    updated some missing .deb dependencies install[.sh] scripts..

--- bin/install_browser_agent_dependencies.sh
+++ bin/install_browser_agent_dependencies.sh
@@ -2,6 +2,10 @@
 
 # a crude dependency installation script for browser agent + some infrastructure
 
+script_path=`realpath $0`
+bin_path=`dirname $script_path`
+NAILARA_ROOT=`realpath $bin_path/..`
+
 apt-get update
 
 apt-get -y install pciutils cpanminus libevent-perl libproc-processtable-perl \
@@ -12,12 +16,17 @@ apt-get -y install pciutils cpanminus libevent-perl libproc-processtable-perl \
   libgtk3-perl libx11-protocol-perl libx11-protocol-other-perl \
   libx11-keyboard-perl liblinux-inotify2-perl openbox hsetroot xvfb \
   libgtk3-webkit2-perl libtest-needsdisplay-perl libcryptx-perl \
-  libextutils-pkgconfig-perl libextutils-depends-perl
+  libextutils-pkgconfig-perl libextutils-depends-perl libhttp-message-perl \
+  libhttp-date-perl libdigest-elf-perl
 
-cpanm Crypt::Curve25519 Crypt::Ed25519 \
-      Digest::Skein Glib::Event
+cpanm Crypt::Ed25519 Digest::Skein Glib::Event
 
-cpanm --force POSIX::1003 # <- temporary! ( localtime test fails.. )
+# Crypt::Curve25519 no longer compiles as orig., provided fixed copy locally now
+cpanm $NAILARA_ROOT/lib/pm-src/crypt-curve25519  # until fmul issue fixed upstr.
+cd $NAILARA_ROOT && git clean -fxd $NAILARA_ROOT/lib/pm-src
+
+cpanm --force POSIX::1003      # <- temporary <!> ( localtime test fails .. )
+cpanm --force XML::RSS::Timing # <- temporary forced [date_conv test fails] <!>
 
 #>-- HTTP::Soup
 export PERL5LIB=.
