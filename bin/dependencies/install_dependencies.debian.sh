8cf4f6a845b653cb4a19571e26b723193f6e3f67
Taeki Ten of {[.NAiLArA:T3K`]} <taeki.ten@protocol-7.network>
2020-12-07 09:44:35 +0100

    updated 'install_dependencies.debian.sh' script

--- bin/dependencies/install_dependencies.debian.sh
+++ bin/dependencies/install_dependencies.debian.sh
@@ -6,7 +6,7 @@
 apt-get -y install notify-osd x11-utils x11-xserver-utils unclutter xnest nxagent \
             compton openbox hsetroot xserver-xorg xserver-xephyr xvfb fuse3 \
             mpv ffmpeg melt expect mediainfo dbus-x11 libnotify-bin notify-osd \
-            python-pil python-pygame mupdf-tools pdftk \
+            python-pil python-pygame mupdf-tools pdftk libpoppler-glib-dev \
             libio-socket-multicast-perl gnome-colors-common oxygen-icon-theme \
             liblwpx-paranoidagent-perl libnet-libdnet-perl libtimedate-perl \
             libc6-dev libimlib2-dev liblinux-inotify2-perl libimager-perl \
@@ -17,7 +17,8 @@ apt-get -y install notify-osd x11-utils x11-xserver-utils unclutter xnest nxagen
             libdigest-elf-perl libyaml-libyaml-perl libbsd-resource-perl
 
 cpanm Glib::Event
-cpanm --force XML::RSS::Timing  # <- temporary forced [date_conv test fails] <!>
+cpanm --force XML::RSS::Timing  # <-- temporary forced [ date_conv test fails ]
+cpanm --force XML::RSS::TimingBot  # <-- evaluate if above module still required
 
 # optional [i.e. not on raspbian]
 apt-get -y install intel-gpu-tools 2>/dev/null
