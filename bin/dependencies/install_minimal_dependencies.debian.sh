25c3557af7ebbd81cb8103d9081c45e1ba92fc3a
Taeki Ten of {[.NAiLArA:T3K`]} <taeki.ten@protocol-7.network>
2021-02-06 04:06:30 +0100

    renamed agent 'tradix' --> 'c-trade'

--- bin/dependencies/install_minimal_dependencies.debian.sh
+++ bin/dependencies/install_minimal_dependencies.debian.sh
@@ -4,11 +4,11 @@ script_path=`realpath $0`
 bin_path=`dirname $script_path` # ./bin/dependencies/
 NAILARA_ROOT=`realpath $bin_path/../..`
 
-useradd -m -r nailara
-ln -v -f -s $NAILARA_ROOT/bin/protocol-7 /usr/local/bin/protocol-7
-ln -v -f -s $NAILARA_ROOT/bin/nshell /usr/local/bin/nshell
+( id nailara || useradd -m -r nailara ) 2>&1 >/dev/null
+ln -f -s $NAILARA_ROOT/bin/protocol-7 /usr/local/bin/protocol-7
+ln -f -s $NAILARA_ROOT/bin/nshell /usr/local/bin/nshell
 
-cp -v $NAILARA_ROOT/lib/systemd/system/protocol-7.service /lib/systemd/system/
+cp $NAILARA_ROOT/lib/systemd/system/protocol-7.service /lib/systemd/system/
 
 # dependencies for 'nroot','core','config', 'events' agents + nshell
 
@@ -21,15 +21,27 @@ apt-get -y install cpanminus git libevent-perl libproc-processtable-perl \
   libdigest-jhash-perl libdigest-elf-perl libhttp-date-perl libnet-dns-perl \
   libtest-requires-perl libtest-sharedfork-perl libhttp-message-perl \
   libfile-which-perl libyaml-libyaml-perl libfile-extattr-perl \
-  libbsd-resource-perl &&
+  libbsd-resource-perl liburi-query-perl libconfig-simple-perl \
+  libfile-finder-perl libmce-perl libperl-critic-perl libppix-utilities-perl &&
 
 # no longer found: libmodule-build-pluggable-perl
 
 cpanm Crypt::Ed25519 Digest::Skein Digest::BMW Net::IP::Lite \
-      File::MimeInfo::Magic Sys::Statistics::Linux::CpuStats URI::QueryParam &&
+      File::MimeInfo::Magic Sys::Statistics::Linux::CpuStats URI::QueryParam
 
 # Crypt::Curve25519 no longer compiles as orig., provided fixed copy locally now
-cpanm $NAILARA_ROOT/lib/pm-src/crypt-curve25519  # until fmul issue fixed upstr.
-cd $NAILARA_ROOT && git clean -fxd $NAILARA_ROOT/lib/pm-src
+#                                                until fmul issue fixed upstream
+perl -Mv5.13 -e \
+ 'my$M="Crypt::Curve25519";eval"require $M";exit(1)if$@;say"$M is installed.,"'||
+  cpanm $NAILARA_ROOT/lib/pm-src/crypt-curve25519 &&
+    cd $NAILARA_ROOT && git clean -fxd $NAILARA_ROOT/lib/pm-src
+
+# 'c-trade'-agent requirements..,
+perl -Mv5.13 -e \
+ 'my$M="Poloniex::API";eval"require $M";exit(1)if$@;say"$M is installed .,"'||
+cpanm --force Poloniex::API
+perl -Mv5.13 -e \
+ 'my$M="Bittrex::API";eval"require $M";exit(1)if$@;say"$M is installed ..,"'||
+cpanm https://github.com/jheddings/bittrex.git
 
 # LLL cpan2deb --recursive --build --install-deps --install-build-deps --install
