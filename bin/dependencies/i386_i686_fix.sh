05b46224ec533a2a4309ab0e684f61045126e236
Taeki Ten of {[.NAiLArA:T3K`]} <sourcecode@nailara.network>
2021-02-10 09:35:38 +0100

    now also signing code in './bin/dev' and './bin/dependencies'

--- bin/dependencies/i386_i686_fix.sh
+++ bin/dependencies/i386_i686_fix.sh
@@ -3,3 +3,8 @@
 # temporary fixes to compile on i386 systems
 
 cpanm https://github.com/anotherlink/crypt-curve25519.git # renamed 'fmul', worx
+# ______________________________________________________________________________
+#\\Z3FKDP4S2BNGESBJXAR3HSG7MXRNSVXPV56ZEH2RQSMLW7ORSQB5VMKUECJNGEIAYXMOOWFE3Z4CW
+# \\ R2MB2UA6BHNQU4O5T4FDYHMY6REFO2CQSFPNMGP4RKDPR2S3ZFS5 \\// C25519-BASE-32 //
+#  \\// ELTSD3NEXKEMI7J5UXZBECBJIAVRUBL3MBUTXCE7TK6FG47QYBI \\ CODE SIGNATURE \\
+#   ````````````````````````````````````````````````````````````````````````````
