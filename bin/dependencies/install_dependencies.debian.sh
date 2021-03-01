063e081869e0d0713dc2b5d67f5533ffe6fb7188
{[.NAiLArA:T3K`]} <sourcecode@nailara.network>
2021-03-01 03:42:01 +0100

    added missing 'libgtk3-webkit2-perl' to 'install_dependencies.debian.sh'

--- bin/dependencies/install_dependencies.debian.sh
+++ bin/dependencies/install_dependencies.debian.sh
@@ -4,19 +4,19 @@
 #         #####################################
 
 apt-get -y install mpv melt xvfb fuse3 pdftk xnest expect ffmpeg compton \
-            nxagent openbox dbus-x11 hsetroot libc6-dev mediainfo unclutter \
-            x11-utils notify-osd notify-osd mupdf-tools python3-pil \
-            libgtk3-perl xserver-xorg libimlib2-dev libnotify-bin \
-            libimager-perl python3-pygame xserver-xephyr libtimedate-perl \
-            shared-mime-info libhttp-date-perl oxygen-icon-theme \
-            x11-xserver-utils libdigest-crc-perl libdigest-elf-perl \
-            libio-stringy-perl gnome-colors-common libnet-libdnet-perl \
-            libpoppler-glib-dev libxml-rsslite-perl libbsd-resource-perl \
-            libdigest-jhash-perl libfile-slurper-perl libhttp-message-perl \
-            libyaml-libyaml-perl libconvert-color-perl libfile-mimeinfo-perl \
-            libfont-freetype-perl liblinux-inotify2-perl \
-            libtest-needsdisplay-perl liblwpx-paranoidagent-perl \
-            libio-socket-multicast-perl
+           nxagent openbox dbus-x11 hsetroot libc6-dev mediainfo unclutter \
+           x11-utils notify-osd notify-osd mupdf-tools python3-pil \
+           libgtk3-perl xserver-xorg libimlib2-dev libnotify-bin \
+           libimager-perl python3-pygame xserver-xephyr libtimedate-perl \
+           shared-mime-info libhttp-date-perl oxygen-icon-theme \
+           x11-xserver-utils libdigest-crc-perl libdigest-elf-perl \
+           libio-stringy-perl gnome-colors-common libnet-libdnet-perl \
+           libpoppler-glib-dev libxml-rsslite-perl libbsd-resource-perl \
+           libdigest-jhash-perl libfile-slurper-perl libgtk3-webkit2-perl \
+           libhttp-message-perl libyaml-libyaml-perl libconvert-color-perl \
+           libfile-mimeinfo-perl libfont-freetype-perl \
+           liblinux-inotify2-perl libtest-needsdisplay-perl \
+           liblwpx-paranoidagent-perl libio-socket-multicast-perl
 
 cpanm Glib::Event
 cpanm --force XML::RSS::Timing  # <-- temporary forced [ date_conv test fails ]
@@ -26,7 +26,7 @@ cpanm --force XML::RSS::TimingBot  # <-- evaluate if above module still required
 apt-get -y install intel-gpu-tools firmware-misc-nonfree 2>/dev/null
 
 # ______________________________________________________________________________
-#\\IWBFTKGKEW4BMLHJFYSCK5U6PC3YGA2TI5NXHSGUB5V3QCJBJKEZCLDCTSCJRJOCQBNP56NIP6MIU
-# \\ U6DU4FSJTQDJW5KSRWQDNDFUMTONH3DEZ2HTD7GEMS377LNPRNH4 \\// C25519-BASE-32 //
-#  \\// TF5ML4STNKTGLLAKF3CEHAEQVO4AQXFQOHYNRB67IKLQORTTKCQ \\ CODE SIGNATURE \\
+#\\WX63FBARSQJ3AGHR5G5IZC4EXMLWMXG2DE2DC57Y5Q2CONU5SCMZSWK4N2QRWSZ33EIFXY3MLK7X2
+# \\ Y7LDYDNGLA6VTX5HDJP33DNKT74ZOQW4IZ5B5S4GXXEF5P2HG7MD \\// C25519-BASE-32 //
+#  \\// 3TOOEAKFYSIPD7NEMBK72C2C5XVDQNBILXUTAEBL4MXOB5PMUBY \\ CODE SIGNATURE \\
 #   ````````````````````````````````````````````````````````````````````````````
