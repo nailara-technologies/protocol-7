74c01b5862d2573d0d7dac5487efb2f154c04d09
Taeki Ten of {[.NAiLArA:T3K`]} <sourcecode@nailara.network>
2021-03-26 04:51:15 +0100

    renamed code library path './lib/' --> './data/lib-path'

--- bin/dependencies/install_minimal_dependencies.debian.sh
+++ bin/dependencies/install_minimal_dependencies.debian.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 script_path=`realpath $0`
-bin_path=`dirname $script_path` # ./bin/dependencies/
+bin_path=`dirname $script_path` # ./bin/dependencies
 NAILARA_ROOT=`realpath $bin_path/../..`
+EXT_LIB_PATH="$NAILARA_ROOT/data/lib-path"
 
 ln -f -s $NAILARA_ROOT/bin/protocol-7 /usr/local/bin/protocol-7
 ln -f -s $NAILARA_ROOT/bin/nshell /usr/local/bin/nshell
 
-cp $NAILARA_ROOT/lib/systemd/system/protocol-7.service /lib/systemd/system/
+cp $EXT_LIB_PATH/systemd/system/protocol-7.service /lib/systemd/system/
 
 # dependencies for 'nroot','core','config', 'events' agents + nshell
 
@@ -29,23 +30,23 @@ apt-get -y install gcc git make cpanminus libc6-dev libc6-dev libmce-perl \
 
 # no longer found: libmodule-build-pluggable-perl
 
-cpanm Crypt::Ed25519 Digest::Skein Digest::BMW Net::IP::Lite SigAction::SetCallBack \
-      File::MimeInfo::Magic Sys::Statistics::Linux::CpuStats URI::QueryParam
+cpanm Crypt::Ed25519 Digest::Skein Digest::BMW Net::IP::Lite URI::QueryParam \
+  File::MimeInfo::Magic Sys::Statistics::Linux::CpuStats SigAction::SetCallBack
 
 # Crypt::Curve25519 no longer compiles as orig., provided fixed copy locally now
 #                                                until fmul issue fixed upstream
 perl -Mv5.13 -e \
- 'my$M="Crypt::Curve25519";eval"require $M";exit(1)if$@;say"$M is installed.,"'||
-  cpanm $NAILARA_ROOT/lib/pm-src/crypt-curve25519 &&
-    cd $NAILARA_ROOT && git clean -fxd $NAILARA_ROOT/lib/pm-src
+'my$M="Crypt::Curve25519";eval"require $M";exit(1)if$@;say"$M is installed.,"'||
+  cpanm $NAILARA_ROOT/$EXT_LIB_PATH/pm-src/crypt-curve25519 &&
+    cd $NAILARA_ROOT && git clean -fxd $EXT_LIB_PATH/pm-src
 
 # 'c-trade'-agent requirements..,
 perl -Mv5.13 -e \
- 'my$M="Poloniex::API";eval"require $M";exit(1)if$@;say"$M is installed .,"'||
-cpanm --force Poloniex::API
+  'my$M="Poloniex::API";eval"require $M";exit(1)if$@;say"$M is installed .,"'||
+  cpanm --force Poloniex::API
 perl -Mv5.13 -e \
- 'my$M="Bittrex::API";eval"require $M";exit(1)if$@;say"$M is installed ..,"'||
-cpanm --force https://github.com/jheddings/bittrex.git
+  'my$M="Bittrex::API";eval"require $M";exit(1)if$@;say"$M is installed ..,"'||
+  cpanm --force https://github.com/jheddings/bittrex.git
 
 # LLL cpan2deb --recursive --build --install-deps --install-build-deps --install
 
