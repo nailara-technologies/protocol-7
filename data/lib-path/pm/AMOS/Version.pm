d6cc3ce582b113df742abf096acccc65814101e6
Taeki Ten of {[.NAiLArA:T3K`]} <taeki.ten@protocol-7.network>
2021-04-13 12:36:17 +0200

    imported is_true() implementation into AMOS::Assert::Truth

--- data/lib-path/pm/AMOS/Version.pm
+++ data/lib-path/pm/AMOS/Version.pm
@@ -17,7 +17,8 @@ return 1;
 
 sub calc_version {
     ( my $ntime_dec = shift ) =~ s|\D+||;
-    my $c_version = sprintf "%.3f", $ntime_dec / 7**16;
+    my $c_version = sprintf( "%.3f", ( $ntime_dec / 7**14 ) - 3.6 ); #[magic]#
+
     $c_version =~ s|\d{2}$|\.$MATCH|;
     $c_version =~ s|0$||;
     return $c_version;
@@ -33,3 +34,9 @@ sub calc_ntime {    # [ network time : secs from 2002-06-05 * 4200 ]
 
     return $ntime;
 }
+
+#.............................................................................
+#MNDI7BWDVX3FE7BUB5S2WBDGCP3TSPLEDHBLYVM4YSCYSSLKRRB43L3EW75XE6W4L4ECLJ6453UE4
+#::: ATLJUDEVH5OG2O2DUD447NIZMG4R74BFU7266WGNPQB4GZHLDD5 :::: NAILARA AMOS :::
+# :: QFIS4QDYBM4DGU3JFIMHEBKPYAN3KKTI2RQ5TG3ID6SMWSKF3UCY :: CODE SIGNATURE ::
+# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
