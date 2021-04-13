b467e6545caf151c997fc62fa996f88058264362
Taeki Ten of {[.NAiLArA:T3K`]} <taeki.ten@protocol-7.network>
2021-04-13 12:36:17 +0200

    imported is_true() implementation into AMOS::Assert::Truth

--- data/lib-path/pm/AMOS/Assert.pm
+++ data/lib-path/pm/AMOS/Assert.pm
@@ -14,3 +14,9 @@ use base qw| Exporter |;
 @EXPORT = qw|  |;
 
 return 1;
+
+#.............................................................................
+#ISGWQWB7DBLKJR4WXK7HL2XV7OF53KNVBHTDFSXADGRDXKUFCCZWEQNIN5SAFUG6WRIWHNLXSURFC
+#::: RXZWFDRTMBV6Z7BUWFVNBKQ6P6YKO2XRLZR2PQWQAGTVBLCQXHH :::: NAILARA AMOS :::
+# :: DRRIX5PGEKAV3U2DRME5AN4QA6SENENR4YPWOFPE5DMXSM6TS6BI :: CODE SIGNATURE ::
+# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
