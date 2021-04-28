178be43854236a7bada9b8fb5ad4222f9bb75e0c
Taeki Ten of {[.NAiLArA:T3K`]} <taeki.ten@protocol-7.network>
2021-04-28 06:16:49 +0200

    committed 'file-matrix.map-dirs' stub-code structure

---
+++ base-code/file-matrix.map-dirs.fs
@@ -0,0 +1,9 @@
+# >:]
+
+# name  = file-matrix.map-dirs.fs
+
+#.............................................................................
+#6DNUPNJBBKW62R5RYTL4MXQ6VUBISWJYW66LZHUJPRHL3GBX3JWOB4GCZYOPFOBYUTL4X4RPETVAO
+#::: PQXMT2YVHAWQGCADW6DWPHLJ6SKZNK2XEUCKSA5ZXG2R4FINCI2 :::: NAILARA AMOS :::
+# :: RUKSE2VAH4HO6KZWNDYFCUWE3KHEXYQOAVTZSCTIC5PFLQ4ASQCY :: CODE SIGNATURE ::
+# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
