7b53fe128409ea78a51e9dfcaaef691c3cfcedf5
Taeki Ten of {[.NAiLArA:T3K`]} <taeki.ten@protocol-7.network>
2021-05-20 10:54:25 +0200

    clean-up and signatures in 'bin/dev/*'

--- bin/dev/script-scratchpad/udt_test_client.pl
+++ bin/dev/script-scratchpad/udt_test_client.pl
@@ -20,3 +20,9 @@ $u->sendmsg($message);
 # if it is SOCK_DGRAM the whole message will be sent
 # $u->send($message);
 $u->close();
+
+#.............................................................................
+#2XWZNR5URUUUGWVW3G3CDNLYN2PX7CKL4JX7PPNBYB7WU56P7K4HGN3KYS43S2BMNY7VZSJKJICDI
+#::: WHARPIOQU65LDJXNI55UXXW5SUVM4XTD7EB3YMC7EKUTRZVZC2J :::: NAILARA AMOS :::
+# :: PKEQNAQ5IDYFOE7I6OM5DV7VZ7A4KYWAOEUW3ZQHODJPLBYD6GCA :: CODE SIGNATURE ::
+# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
