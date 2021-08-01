6590992b0d0bb73365b6a2122e9b3eb733b0edb4
Taeki Ten of {[.NAiLArA:T3K`]} <sourcecode@nailara.network>
2021-08-01 06:00:32 +0200

    implemented 'AMOS7::Twofish' module

---
+++ data/lib-path/pm/AMOS7/Twofish.pm
@@ -0,0 +1,169 @@
+
+package AMOS7::Twofish; ######################################################
+
+use v5.24;
+use utf8;
+use strict;
+use English;
+use warnings;
+
+use AMOS7;
+use Crypt::Twofish2;
+
+use vars qw| $VERSION @EXPORT @EXPORT_OK |;
+
+use Exporter;
+use base qw| Exporter |;
+
+@EXPORT = qw[ ];    ##  none  ##
+
+@EXPORT_OK = qw| key_init object_table delete_table_entry encrypt decrypt |;
+
+my $VERSION = qw| AMOS7::Twofish-VERSION.2EXJJ7Q |;
+
+##[ INIT ]####################################################################
+
+our %encryption_table;
+our %decryption_table;
+
+our $key_length     = 32;
+our $min_block_size = 16;
+
+##[ INIT OBJECT ]#############################################################
+
+sub key_init {
+    my $crypt_key = shift;
+    my $o_type    = shift;
+    my $o_name    = shift;
+
+    if ( length( $crypt_key // '' ) != $key_length ) {
+        warn_err( 'expected twofish key [ length %03d octets ] <{C1}>',
+            $key_length );
+        return undef;
+    } elsif ( not length( $o_type // '' ) ) {
+        warn_err('expected twofish object type <{C1}>');
+        return undef;
+    } elsif ( not length( $o_name // '' ) ) {
+        warn_err('expected twofish object name <{C1}>');
+        return undef;
+    } elsif ( $o_type eq qw| encryption | ) {
+        $encryption_table{$o_name}    ##  encryption table  ##
+            = Crypt::Twofish2->new( $crypt_key, Crypt::Twofish2::MODE_CBC );
+    } else {
+        $decryption_table{$o_name}    ##  decryption table  ##
+            = Crypt::Twofish2->new( $crypt_key, Crypt::Twofish2::MODE_CBC );
+    }
+    ## true ##
+    return 5;
+}
+
+##[ LIST OBJECTS ]############################################################
+
+sub object_table {
+    return {
+        qw| encryption | => [
+            sort { length $a <=> length $b }
+                reverse sort keys %encryption_table
+        ],
+        qw| decryption | => [
+            sort { length $a <=> length $b }
+                reverse sort keys %decryption_table
+        ]
+    };
+}
+
+##[ DELETE OBJECT ]###########################################################
+
+sub delete_table_entry {
+    my $o_type = shift;
+    my $o_name = shift;
+
+    if ( not length( $o_type // '' ) ) {
+        warn_err('expected twofish object type <{C1}>');
+        return undef;
+    } elsif ( not length( $o_name // '' ) ) {
+        warn_err('expected twofish object name <{C1}>');
+        return undef;
+    } elsif ( $o_type eq qw| encryption | ) {
+        if ( not defined $encryption_table{$o_name} ) {
+            warn_err( 'twofish encryption object %s not defined <{C1}>',
+                1, $o_name );
+            return undef;
+        } else {    ##  encryption table  ##
+            $encryption_table{$o_name} = undef;
+            delete $encryption_table{$o_name};
+        }
+    } else {    ##  decryption table  ##
+        if ( not defined $decryption_table{$o_name} ) {
+            warn_err( 'no twofish decryption object %s defined <{C1}>',
+                1, $o_name );
+            return undef;
+        } else {
+            $decryption_table{$o_name} = undef;
+            delete $decryption_table{$o_name};
+        }
+    }
+    return 5;    ## true ##
+}
+
+##[ ENCRYPT ]#################################################################
+
+sub encrypt {
+    my $o_name           = shift // '';
+    my $payload_data_ref = shift;
+
+    if ( not length( $o_name // '' ) ) {
+        warn_err('expected twofish object name <{C1}>');
+        return undef;
+    } elsif ( not defined $encryption_table{$o_name} ) {
+        warn_err( "no such object defined [ '%s' ] <{C1}>", 1, $o_name );
+        return undef;
+    } elsif ( ref $payload_data_ref ne qw| SCALAR | ) {
+        warn_err('expected scalar reference parameter to payload data');
+        return undef;
+    } elsif ( not length( $payload_data_ref->$* )
+        or length( $payload_data_ref->$* ) % 16 != 0 ) {
+        warn_err('payload data needs to be multiple of 16 bytes');
+        return undef;
+    }
+
+    my $encrypted_data
+        = $encryption_table{$o_name}->encrypt( $payload_data_ref->$* );
+
+    return \$encrypted_data;
+}
+
+##[ DECRYPT ]#################################################################
+
+sub decrypt {
+    my $o_name           = shift // '';
+    my $payload_data_ref = shift;
+
+    if ( not length( $o_name // '' ) ) {
+        warn_err('expected twofish object name <{C1}>');
+        return undef;
+    } elsif ( not defined $decryption_table{$o_name} ) {
+        warn_err( "no such object defined [ '%s' ] <{C1}>", 1, $o_name );
+        return undef;
+    } elsif ( ref $payload_data_ref ne qw| SCALAR | ) {
+        warn_err('expected scalar referdece parameter to payload data');
+        return undef;
+    } elsif ( not length( $payload_data_ref->$* )
+        or length( $payload_data_ref->$* ) % 16 != 0 ) {
+        warn_err('payload data needs to be multiple of 16 bytes');
+        return undef;
+    }
+
+    my $decrypted_data
+        = $decryption_table{$o_name}->decrypt( $payload_data_ref->$* );
+
+    return \$decrypted_data;
+}
+
+return 5;  ###################################################################
+
+#,,,,,.,.,,..,.,.,..,,...,.,.,..,,...,...,,,,,..,,...,...,.,,,,,.,.,,,,,.,,.,,
+#JPA4I7ZNL7XNOAQQN2HGLI4DZTXQ75B5OQ76XCF5J2JHMCFK6ET7BLKUB7GGCAEK3VUKFRSM3HME6
+#\\\|ZC6VWB4SINXVRXT5VI7LGLT5SNQ3BIEST3DI5JY56FOQF3SM7HN \ / AMOS7 \ YOURUM ::
+#\[7]56GB5BYBWSBOZT25VJJ3BJOBU7CZJAVP6UHAGFDFD5WJFUUMJUBI 7  DATA SIGNATURE ::
+#:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
--- read-me/md/README.md
+++ read-me/md/README.md
@@ -1,15 +1,15 @@
 
 # [ [nailara 'protocol seven' project](http://nailara.network/) ]
 
-### [ source code version : ZHZPJLFMDA-4629.0 ]
+### [ source code version : ZHZY5WNRFY-4630.0 ]
 
 ### ( this is the public domain [license](../license)d 'base' branch )
 ---
 ## current [release](https://github.com/nailara-technologies/protocol-7/releases) \\\\// [AMOS7-v2.27.9](https://github.com/nailara-technologies/protocol-7/releases/tag/AMOS7-v2.27.9)
 ---
 
-#,,..,...,,..,...,.,,,,,.,.,.,.,.,.,,,.,,,,,.,..,,...,...,,,.,,.,,,..,.,.,,,.,
-#CWA5VRZB4XMNLVXI5NOTQWYISUIFBIB7XAMLPMIJVYL34R3H4VGA65PYYEBEJTJY4WBUET6VDJK4C
-#\\\|GIVCNAMOJKSVEF5SSAQ4BOGL7AAITCDSTC6PE45EGE46DVD2HVY \ / AMOS7 \ YOURUM ::
-#\[7]SF2WTINW5F3W2YFATBOIUQCQZ5LJIUUWX7QFIPC4HMCSVXHOCOBI 7  DATA SIGNATURE ::
+#,,.,,,.,,,,,,.,,,.,,,...,..,,..,,,,.,..,,...,..,,...,..,,,..,.,,,.,.,,,,,,..,
+#A3QVIM5DZA2WBZTS3IT23FZO6GK3ZNDHJL5BMQXJSPKDCWNX3RKO25UIYN53RJEUOCAOKZFIT4I42
+#\\\|XHEZ25WSQFTSP6RZ3RWXPVFB3IX6TGUEOKO7YZYOZDROVR2LEHV \ / AMOS7 \ YOURUM ::
+#\[7]STLYCF4TBKQZ2BTFZRJOXU5TNVYV6OPPIYN37KAX2BN7BUPDTSAQ 7  DATA SIGNATURE ::
 #:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
