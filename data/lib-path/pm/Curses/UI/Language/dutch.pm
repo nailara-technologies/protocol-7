0a78f90f9e0eb1c22a456e3210b1eaae9e8d7568
Taeki Ten of {[.NAiLArA:T3K`]} <taeki.ten@protocol-7.network>
2021-08-07 07:37:32 +0200

    imported 'Curses::UI' perl module to './data/lib-path/pm'

---
+++ data/lib-path/pm/Curses/UI/Language/dutch.pm
@@ -0,0 +1,80 @@
+# ----------------------------------------------------------------------
+# Curses::UI::Language::dutch
+# Maintainer: maurice@gitaar.net
+# ----------------------------------------------------------------------
+
+package Curses::UI::Language::dutch;
+1;
+__DATA__
+
+# ----------------------------------------------------------------------
+# For Curses::UI
+# ----------------------------------------------------------------------
+
+[screen_too_small]
+Uw scherm is momenteel te klein voor deze applicatie.
+Maak het scherm groter en herstart de applicatie.
+Druk op <CTRL+C> om het programma te verlaten...
+
+# ----------------------------------------------------------------------
+# For Curses::UI::Calendar
+# ----------------------------------------------------------------------
+
+[months]
+Januari Februari Maart April Mei Juni Juli
+Augustus September Oktober November December
+
+[days_short] Zo Ma Di Wo Do Vr Za
+
+# ----------------------------------------------------------------------
+# For Curses::UI::Buttonbox
+# ----------------------------------------------------------------------
+
+[button_ok]      o:OK
+[button_cancel]  a:Annuleren
+[button_yes]     j:Ja
+[button_no]      n:Nee
+
+# ----------------------------------------------------------------------
+# For Curses::UI::Dialog::Error
+# ----------------------------------------------------------------------
+
+[error_title]    Foutmelding
+
+# ----------------------------------------------------------------------
+# For Curses::UI::Dialog::FileBrowser
+# ----------------------------------------------------------------------
+
+# The filebrowser title
+[file_title]     Selecteer bestand
+[file_savetitle] Selecteer bestand om te schrijven
+[file_loadtitle] Selecteer bestand om te lezen
+
+# The labels for the dialog screen.
+[file_path]      Pad     :
+[file_file]      Bestand :
+[file_mask]      Masker  :
+
+# The size of the longest label
+[file_labelsize] 9
+
+# On directory up
+[file_dirup] Directory hoger
+
+# For asking the user if a file may be overwritten
+[file_overwrite_title] Bevestiging
+[file_overwrite_question_pre]
+Weet u zeker dat u het bestand "
+[file_overwrite_question_post]
+" wilt overschrijven?
+
+# Errors
+[file_err_opendir_pre] Kan directory "
+[file_err_opendir_post] " niet openen
+[file_err_nofileselected] U heeft nog geen bestand geselecteerd!
+
+#,,.,,.,.,,,,,.,,,,,,,,,,,,,,,,,.,...,,.,,..,,..,,...,..,,,..,...,,,,,..,,..,,
+#QJBKFI4VRB35E6JJ7VG7O4TQRNZZ3EBQTYMZGF4LGASOXXCWF3BV4XINTOVLJREVS3OKYBSNSKAHG
+#\\\|B3WSKX42C5BCKANHLHFMY6QBRCCA7LQUSX2IQ5H7RGD2VMT5ZDJ \ / AMOS7 \ YOURUM ::
+#\[7]SMTUBL44H6GSU76XUMZ43GBW4IX7LY6O7RP2CG6TIZ4SGBISMCDQ 7  DATA SIGNATURE ::
+#:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
--- read-me/md/README.md
+++ read-me/md/README.md
@@ -1,15 +1,15 @@
 
 # [ [nailara 'protocol seven' project](http://nailara.network/) ]
 
-### [ source code version : ZH52H2NXLY-4670.0 ]
+### [ source code version : ZH52LVUFKY-4671.0 ]
 
 ### ( this is the public domain [license](../license)d 'base' branch )
 ---
 ## current [release](https://github.com/nailara-technologies/protocol-7/releases) \\\\// [AMOS7-v2.27.9](https://github.com/nailara-technologies/protocol-7/releases/tag/AMOS7-v2.27.9)
 ---
 
-#,,..,,,.,...,.,,,..,,.,,,,.,,,,,,.,,,,,,,.,,,..,,...,...,...,,.,,,,,,,.,,...,
-#HPZN4LIQS7BYXWKYPJIQU4ZBZ6PQ6CD5OG4BBXTZPIEDET5SOMQJMKHUYCJYQQ3P26NSWSRSULINK
-#\\\|GLUOZSOAL5BN6GJES75PWFHVYOI6GKIB64M64MKVSEWC2NZ3XAP \ / AMOS7 \ YOURUM ::
-#\[7]NNGUDPGM343IVJMJYVLEONTWYSXMIQ3VSZDGEIH3CUVTZYLS5SDQ 7  DATA SIGNATURE ::
+#,,,.,,,,,,,,,.,.,,..,,..,.,,,,,,,,,,,...,,,.,..,,...,..,,.,.,.,.,,,.,,,,,,..,
+#3AE3WSBVXFX6GSVLIODMFJKHPUB5PO3LILCCMIOTN26NVVIFFA75HOUNSQH6PIFDXMRHMXBH3PKFI
+#\\\|OUVOMGYILPTLEVWY7FXQN3CDB57KZZIMPJTDHIHHVBPSZYWDVE2 \ / AMOS7 \ YOURUM ::
+#\[7]CY3LHA77NDYUG4SUAP6NNCZO2MXEQOTJBNSEG6ZDPXQXAZN7KYDY 7  DATA SIGNATURE ::
 #:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
