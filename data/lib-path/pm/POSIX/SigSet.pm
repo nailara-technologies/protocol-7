d7733deb51fdb9f9c468b0377eaf22f93fe5f358
Taeki Ten of {[.NAiLArA:T3K`]} <sourcecode@nailara.network>
2021-08-22 09:49:31 +0200

    included POSIX::SigAction and POSIX::SigSet in './data/lib-path/pm'

---
+++ data/lib-path/pm/POSIX/SigSet.pm
@@ -0,0 +1,20 @@
+# Copyrights 2011-2020 by [Mark Overmeer].
+#  For other contributors see ChangeLog.
+# See the manual pages for details on the licensing terms.
+# Pod stripped from pm file by OODoc 2.02.
+# This code is part of distribution POSIX-1003.  Meta-POD processed with
+# OODoc into POD and HTML manual-pages.  See README.md
+# Copyright Mark Overmeer.  Licensed under the same terms as Perl itself.
+
+use warnings;
+use strict;
+
+# Implemented in XS
+
+1;
+
+#,,..,..,,...,,,,,,,,,,,.,.,,,...,.,.,,..,..,,..,,...,..,,..,,.,.,.,.,...,.,,,
+#SRUSFVVLNOPVVPHY64KGUGQE72M2PAO2UX2WIRPLY7HDDNI6MA6KHDNVWSQOVERG6KNP3DE4V433M
+#\\\|ZTXVE4EKOLOZVSNN7YGHBVAGVVWKHWV4G4R5FSEMEFHLG5PGRN3 \ / AMOS7 \ YOURUM ::
+#\[7]CNELX5OJXE42CBOIKQT3DJNVXINKNDM3J7TPDNON54G3YBDGGWCA 7  DATA SIGNATURE ::
+#:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
