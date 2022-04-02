c2a93c32e0094fbb5a21c6fc8fbf55743a17fb47
Taeki Ten of {[.NAiLArA:T3K`]} <taeki.ten@protocol-7.network>
2022-04-02 21:11:47 +0200

    updated 'install_minimal_dependencies.debian.sh'

--- bin/dependencies/install_minimal_dependencies.debian.sh
+++ bin/dependencies/install_minimal_dependencies.debian.sh
@@ -5,7 +5,7 @@ bin_path=`dirname $script_path` # ./bin/dependencies
 NAILARA_ROOT=`realpath $bin_path/../..`
 EXT_LIB_PATH="$NAILARA_ROOT/data/lib-path"
 
-ln -f -s $NAILARA_ROOT/bin/protocol-7 /usr/local/bin/protocol-7
+ln -f -s $NAILARA_ROOT/bin/Protocol-7 /usr/local/bin/Protocol-7
 ln -f -s $NAILARA_ROOT/bin/nshell /usr/local/bin/nshell
 
 cp $EXT_LIB_PATH/systemd/system/Protocol-7.service /lib/systemd/system/
@@ -21,12 +21,13 @@ apt-get -y install gcc git make cpanminus libc6-dev libc6-dev libmce-perl \
             libbsd-resource-perl libdigest-jhash-perl libfile-extattr-perl \
             libfile-slurper-perl libhash-flatten-perl libhttp-message-perl \
             libyaml-libyaml-perl libconfig-simple-perl libio-socket-ssl-perl \
-            libtest-requires-perl libppix-utilities-perl \
+            libtest-requires-perl libppix-utilities-perl shared-mime-info \
             libtest-exception-perl libtest-sharedfork-perl \
             libhash-merge-simple-perl libproc-processtable-perl \
             libterm-readline-gnu-perl libterm-readpassword-perl \
             liblwp-protocol-https-perl libclass-accessor-lite-perl \
-            libio-socket-multicast-perl &&
+            libio-socket-multicast-perl libinline-c-perl libconst-fast-perl \
+            libio-compress-perl &&
 
 # no longer found: libmodule-build-pluggable-perl
 
@@ -36,7 +37,10 @@ apt-get -y install libterm-size-perl \
   libunicode-string-perl libunicode-maputf8-perl
 
 cpanm Crypt::Ed25519 Digest::Skein Digest::BMW Net::IP::Lite URI::QueryParam \
-  File::MimeInfo::Magic Sys::Statistics::Linux::CpuStats SigAction::SetCallBack
+  File::MimeInfo::Magic Sys::Statistics::Linux::CpuStats \
+  SigAction::SetCallBack Crypt-Twofish2
+
+## [ LLL ] ### repair path[?] ###
 
 # Crypt::Curve25519 no longer compiles as orig., provided fixed copy locally now
 #                                                until fmul issue fixed upstream
@@ -55,8 +59,8 @@ perl -Mv5.13 -e \
 
 # LLL cpan2deb --recursive --build --install-deps --install-build-deps --install
 
-#,,,.,,,,,.,.,,..,.,.,.,,,...,.,,,,..,,..,.,.,..,,...,...,...,,..,,..,...,,..,
-#UF7ENKPJPXZKW7M6ASZHPPZUZT47NRVJBBVRJKHGFAP5E3YEGPIQCH4HMDCG7L7M4HZQNXKSJFZWY
-#\\\|MKRY2BY4ZWZZ7ES7J7J32PJNAAMN6OVPAR7GEW5NOLRAV2GT6CG \ / AMOS7 \ YOURUM ::
-#\[7]TDHHZMETAUOIJ6XJJ7K54TU3YQZCJSDSKH5FTC675WKKEZLJLCBI 7  DATA SIGNATURE ::
+#,,..,.,.,.,.,,.,,...,.,,,.,,,,..,...,.,,,,,.,..,,...,...,.,.,,.,,.,,,,..,,.,,
+#BUCK2MMOPDI27QRGRFI6IBWDTKMOWVESOLFMARLM2QHAN4XPW7JMKLCO4XOY3IJL4DXPHK7MAYAU4
+#\\\|FANRFU3AF2PPVW7RIS4A6SAU7KP52L56M5VR6RBH5NE2MXULU32 \ / AMOS7 \ YOURUM ::
+#\[7]GCORPD6S5BMG2NUR2G3MHHFALK7CAZS5MB7ASPY4EAWK55HKPMCQ 7  DATA SIGNATURE ::
 #:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
