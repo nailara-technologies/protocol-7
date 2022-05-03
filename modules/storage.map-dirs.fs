5b8c75a43aab7ab47c8c09b67cd1ee62315bff68
Taeki Ten of {[.NAiLArA:T3K`]} <taeki.ten@protocol-7.network>
2022-05-03 22:56:38 +0200

    renamed zenka : 'file-matrix' --> 'storage'

---
+++ modules/storage.map-dirs.fs
@@ -0,0 +1,9 @@
+## [:< ##
+
+# name  = storage.map-dirs.fs
+
+#,,.,,,..,...,..,,..,,.,,,,.,,..,,,.,,,.,,,,.,..,,...,...,,..,.,.,,.,,,..,,.,,
+#L2ROT7IJQJBF6WUFNIN37XIRSEKPGS2BWESKELLE2NPQFVZTDZTXWG3YTO434BOCGNKUAWFM6PYMK
+#\\\|B6NWUYY4SHYVS33B6UFLWDH7I3R7JRFEAEHG3F5NVWY6W6UYIY3 \ / AMOS7 \ YOURUM ::
+#\[7]NZFLUWLP5UNPAY37B7HROBMTP6RHUPC6RLAEXBOYBDDCAMHPR6CY 7  DATA SIGNATURE ::
+#:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
